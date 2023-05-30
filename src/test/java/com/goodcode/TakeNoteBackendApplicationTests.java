9b4c00f0213029af446842971c0f6687e1869ec2
dc <00danzy@gmail.com>
2023-05-31 03:48:16 +0700

    first commit

---
+++ src/test/java/com/goodcode/TakeNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeNoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
