acfae201065122bbb1ecb11fa0b48e7ffe2f07ec
dc <00danzy@gmail.com>
2023-05-31 03:48:13 +0700

    first commit

---
+++ src/main/java/com/goodcode/user/UserRepository.java
@@ -0,0 +1,18 @@
+package com.goodcode.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+// CHAPTER 2
+
+// 1. Create our `UserRepository` Interface which will be our Data Access Object
+// This repo provides us methods such as findAll, findById e.t.c. all to perform
+// CRUD operations on our User Model we created in CHAPTER 1 (in our User.java)
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    // 2. Add custom method
+    Optional<User> findByEmail(String email);
+}
