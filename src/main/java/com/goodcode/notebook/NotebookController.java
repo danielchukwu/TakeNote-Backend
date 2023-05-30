948ec13436079430840ad6e26a7a2c4a65b0a58d
dc <00danzy@gmail.com>
2023-05-31 03:48:19 +0700

    first commit

---
+++ src/main/java/com/goodcode/notebook/NotebookController.java
@@ -0,0 +1,62 @@
+package com.goodcode.notebook;
+
+import com.goodcode.note.Note;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/notebooks")
+public class NotebookController {
+
+    private final NotebookService notebookService;
+
+    public NotebookController(NotebookService notebookService) {
+        this.notebookService = notebookService;
+    }
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<Notebook> createNotebook(@RequestBody Notebook notebook) {
+        Notebook createdNotebook = this.notebookService.createNotebook(notebook);
+        return ResponseEntity.ok(createdNotebook);
+    }
+
+    // READ
+    @GetMapping("/{id}")
+    public ResponseEntity<Notebook> getNotebook(@PathVariable("id") UUID id) {
+        Optional<Notebook> fetchedNote = this.notebookService.getNotebook(id);
+
+        if (fetchedNote.isPresent()){
+            Notebook notebook = fetchedNote.get();
+            return ResponseEntity.ok(notebook);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping
+    public ResponseEntity<List<Notebook>> getNotebooks() {
+        var notebookList = this.notebookService.getNotebooks();
+        return ResponseEntity.ok(notebookList);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<Notebook> updateNotebook(
+            @PathVariable("id") UUID id,
+            @RequestBody Notebook notebook)
+    {
+        Notebook updateNotebook = this.notebookService.updateNotebook(id, notebook);
+        return ResponseEntity.ok(updateNotebook);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNotebook(@PathVariable("id") UUID id) {
+        this.notebookService.deleteNotebook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
