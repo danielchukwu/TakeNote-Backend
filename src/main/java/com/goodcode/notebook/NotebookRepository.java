aabf6a1dbd17d99b69e3d0d7457d13a24a8fdda6
dc <00danzy@gmail.com>
2023-05-31 03:48:02 +0700

    first commit

---
+++ src/main/java/com/goodcode/notebook/NotebookRepository.java
@@ -0,0 +1,9 @@
+package com.goodcode.notebook;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NotebookRepository extends JpaRepository<Notebook, UUID> {}
