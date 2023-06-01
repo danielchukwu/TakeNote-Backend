282683ba47238bb708b856f662eec31649124a81
dc <00danzy@gmail.com>
2023-06-01 20:28:57 +0100

    add user validation

--- src/main/java/com/goodcode/config/JwtAuthenticationFilter.java
+++ src/main/java/com/goodcode/config/JwtAuthenticationFilter.java
@@ -5,6 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -23,7 +28,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     // without it, we would have an ordinary class. It gives us the method `doFilterInternal(){}`
 
     // 6. Inject JwtService - Allows us check if the token sent is valid and if it matches a user in our database
-    final JwtService jwtService;
+    private final JwtService jwtService;
+
+    // Chap6: 1.
+    private final UserDetailsService userDetailsService;
 
 
     // 2. Override method in OncePerRequest Filter
@@ -31,7 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
-            FilterChain filterChain ) throws ServletException, IOException {
+            FilterChain filterChain) throws ServletException, IOException {
         // 3. Grab Authorization from Header e.g "Authorization": "Bearer kJ3KlAZ2U987dCE232w..."
         final String authHeader = request.getHeader("Authorization");
 
@@ -43,109 +51,74 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         // 5. Grab the jwt token from the Header .ex final String jwt = "kJ3KlAZ2U987dCE232w..."
-        final String jwt_token = authHeader.substring(7);
-        final String userEmail = jwtService.extractEmail(jwt_token);
-
-        // 7. Next we need to add the 3 dependencies (jjwt-api, jjwt-impl, and jjwt-jackson) all from the same package,
-        //    NOTE: Reload Maven project after adding dependencies
-        //
-        // 📁 pom.xml
-        // <dependency>
-        //     <groupId>io.jsonwebtoken</groupId>
-        //     <artifactId>jjwt-api</artifactId>
-        //     <version>0.11.5</version>
-        // </dependency>
-        // <dependency>
-        //     <groupId>io.jsonwebtoken</groupId>
-        //     <artifactId>jjwt-impl</artifactId>
-        //     <version>0.11.5</version>
-        // </dependency>
-        // <dependency>
-        //     <groupId>io.jsonwebtoken</groupId>
-        //     <artifactId>jjwt-jackson</artifactId>
-        //     <version>0.11.5</version>
-        // </dependency>
-
-        // 8.
-        // JWT Explanation
-        //
-        // What is JWT Token
-        // A JSON Web Token is a compact URL SAFE 🔒 means of representing `claims` to be transferred between two parties the client and the server.
-        //
-        //         The claims in JWT are encoded as JSON objects which are literrally signed using a JSON Web signature
-        //
-        // Visit https:// jwt.io/ to see an example representation of an Encoded JWT and A decoded version
-        //
-        // ENCODED FORMAT
-        //
-        // ⌈                                                                                            ⌉
-        //         yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwi
-        //         aWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
-        // ⌊                                                                                            ⌋
-        //
-        //         DECODED FORMAT
-        //
-        // ⌈ HEADER: ALGORITHM & TOKEN TYPE                                                             ⌉
-        //         {
-        //             "alg": "HS256",
-        //                 "typ": "JWT"
-        //         }
-        // ⌊                                                                                            ⌋
-        //
-        // ⌈ PAYLOAD: DATA                                                                              ⌉
-        //         {
-        //             "sub": "1234567890",
-        //                 "name": "John Doe",
-        //                 "iat": 1516239022
-        //         }
-        // ⌊                                                                                            ⌋
-        //
-        // ⌈ VERIFY SIGNATURE                                                                           ⌉
-        //         HMACSHA256(
-        //                 base64UrlEncode(header) + "." +
-        //                         base64UrlEncode(payload),
-        //
-        //                 your-256-bit-secret
-        //         )
-        // ⌊                                                                                            ⌋
-        //
-        // Explanation
-        // The Decoded JWT consists of three parts
-        //
-        // A. HEADER
-        // This consists of
-        //         - The type of signing algorithm used by the token {"alg": "HS256", ...}
-        // - It also holds the type of the token {..., "typ": "JWT"}
-        // ⭐ It is held in the first part of the Token
-        // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9. ------- . ------
-        //
-        //         B. PAYLOAD
-        // The payload consists of the claims. Claims are statements about an Entity, typically the User.
-        // ⭐ It is held in the middle part of the Token
-        // ------. eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9 . ------
-        //
-        // There are 3 types of claims
-        //         - Registered Claims:
-        // These are a set of predefined claims which are not mandatory but recommended to provide
-        // a set of useful claims. ISS, Subject, ODD, EXP etc...
-        // - Public Claims:
-        // These are claims that are defined within the IA & A JSON Web Token Registry or public by
-        //         nature
-        // - Private Claims:
-        // These are custom claims to share information between parties that agree to use them
-        //
-        //
-        // C. VERIFY SIGNATURE
-        // This is used to verify that the center of the jwt token is who they claim to be and it
-        // ensures that the message wasn't changed along the way
-        // ⭐ It is held in the last part of the Token
-        // ------- . ------ . eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-        //
-        //
-        // NOW THAT WE HAVE A BASIC understanding of JWT Tokens. We can start implementing them
+        final String jwt = authHeader.substring(7);
+        final String userEmail = jwtService.extractUsername(jwt);
+        // 7 - 8. Comments are down below
 
 
+        // CHAPTER 6 (Also reference the Diagram at 📁src > main > resources > assets > AppsSecurityArchitecture.png)
+
+        // Chap6: 1. Declare needed field `userDetailsService`
+
+        // User Validation
+        // STEP 1: check if user is already authenticated, if user already is then we don't need to go through the entire
+        //         process of authenticating them again
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            // STEP 2: Call the function we implemented in our ApplicationConfig.java which grabs a user record
+            //         by their username(actually their Email)
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+
+            // STEP 3: If user token is valid. Update the SecurityContextHolder and send the client request to the
+            //         DispatcherServlet and then the DispatcherServlet hands the request to the controller which then
+            //         returns a response back to the client
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+
+                // Grab the AuthToken
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+                // Add additional details to our authToken
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                // Update the SecurityContextHolder
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        // Pass response and request to the next filter in our filter chain
+        filterChain.doFilter(request, response);
     }
+    // NEXT CHAPTER AFTER 7 -> SecurityConfiguration.java
 }
 
 // NEXT: CHAPTER 4 -> JwtService.java
+
+// 7 - 8. Comments
+// 7. Next we need to add the 3 dependencies (jjwt-api, jjwt-impl, and jjwt-jackson) all from the same package,
+//    NOTE: Reload Maven project after adding dependencies
+//
+// 📁 pom.xml
+// <dependency>
+//     <groupId>io.jsonwebtoken</groupId>
+//     <artifactId>jjwt-api</artifactId>
+//     <version>0.11.5</version>
+// </dependency>
+// <dependency>
+//     <groupId>io.jsonwebtoken</groupId>
+//     <artifactId>jjwt-impl</artifactId>
+//     <version>0.11.5</version>
+// </dependency>
+// <dependency>
+//     <groupId>io.jsonwebtoken</groupId>
+//     <artifactId>jjwt-jackson</artifactId>
+//     <version>0.11.5</version>
+// </dependency>
+
+// 8.
+// CHECKOUT THE FILE: JwtExplanation.txt to understand more on Jwt before progressing
+// After that we want to fully implement our JwtService.java file before coming back here to finish this filter up
