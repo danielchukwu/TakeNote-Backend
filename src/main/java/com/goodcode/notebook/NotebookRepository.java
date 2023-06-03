de7b2850d7e7bc21e50ed015c5342c34e8d65f85
dc <00danzy@gmail.com>
2023-06-03 18:56:07 +0100

    make CRU operations only possible for users who own the data - semi milestone 🦜

--- src/main/java/com/goodcode/notebook/NotebookRepository.java
+++ src/main/java/com/goodcode/notebook/NotebookRepository.java
@@ -3,7 +3,10 @@ package com.goodcode.notebook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface NotebookRepository extends JpaRepository<Notebook, UUID> {}
+public interface NotebookRepository extends JpaRepository<Notebook, UUID> {
+    List<Notebook> findAllByUserId(UUID id);
+}
