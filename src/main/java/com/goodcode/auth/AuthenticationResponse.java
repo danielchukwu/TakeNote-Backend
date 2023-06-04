5c8331c0f23d4b98e22b0060abfe9af93d2cb408
dc <00danzy@gmail.com>
2023-06-04 23:18:31 +0100

    add user to auth response

--- src/main/java/com/goodcode/auth/AuthenticationResponse.java
+++ src/main/java/com/goodcode/auth/AuthenticationResponse.java
@@ -5,17 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 
 // CHAPTER 10
 
 // A simple class that has a field token
 
-@Data          // provides setters and getters, equals and haschode methods, toString method e.t.c
+@Data          // provides setters and getters, equals and hashcode methods, toString method e.t.c
 @Builder       // makes this class a builder
 @AllArgsConstructor     // adds a constructor that expects all class fields as args
 @NoArgsConstructor      // adds a constructor with no args
 public class AuthenticationResponse {
 
     private String token;
+    private UserDTO user;
 
 }
+
+record UserDTO(UUID id, String avatar, String name, String email){}
